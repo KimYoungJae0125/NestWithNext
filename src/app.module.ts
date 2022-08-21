e395c1b8bb6a52788ada5fcdb579afc9d6c67d8e
KimYoungJae <uni0125@naver.com>
2022-08-22 01:24:33 +0900

    Add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import Next from 'next';
 import { RenderModule } from 'nest-next';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [RenderModule.forRootAsync(Next({ dev: process.env.NODE_ENV !== 'production' })),],
+  imports: [
+       RenderModule.forRootAsync(Next({
+                                        dev: process.env.NODE_ENV !== 'production'
+                                        })),
+       TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
